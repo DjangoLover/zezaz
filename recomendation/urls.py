36b667c269f2ac50954d08fa238c49699e164379
Ezequiel Bertti <ebertti@gmail.com>
2013-09-28 20:36:46 -0300

    testing social_auth

--- recomendation/urls.py
+++ recomendation/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from recomendation.views.login import OAuthLoginNewView
 
 
 urlpatterns = patterns('recomendation.views',
@@ -7,4 +8,5 @@ urlpatterns = patterns('recomendation.views',
     url(r'^about$', TemplateView.as_view(template_name='about.html'), name="about"),
     url(r'^team$', TemplateView.as_view(template_name='team.html'), name="team"),
 
+    url(r'login/new', OAuthLoginNewView.as_view(), name='login_ok'),
 )
---
+++ recomendation/views/login.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from django.core.urlresolvers import reverse
+from django.views.generic.base import RedirectView
+from django.contrib import messages
+
+
+class OAuthLoginNewView(RedirectView):
+    permanent = False
+    query_string = True
+
+    def get_redirect_url(self):
+        if self.request.user.is_authenticated():
+            messages.add_message(
+                self.request,
+                messages.SUCCESS,
+                "You account was created!"
+            )
+        else:
+            messages.add_message(
+                self.request,
+                messages.ERROR,
+                "Error on creating account"
+            )
+        return reverse('index')
