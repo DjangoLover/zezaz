915d761f31ece3eb3089025bd00a9695a47a6d8c
Ezequiel Bertti <ebertti@gmail.com>
2013-09-28 16:10:47 -0300

    serving with static

--- zezaz/urls.py
+++ zezaz/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 from django.views.generic import TemplateView
 
 admin.autodiscover()
@@ -14,3 +17,6 @@ urlpatterns = patterns('',
     url(r'', include('recomendation.urls', namespace='recomendation'))
 
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
