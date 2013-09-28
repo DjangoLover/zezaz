b681502493d8c71fdac601061b9e4699249d8552
cesarFrias <cagfrias@gmail.com>
2013-09-28 16:46:54 -0300

    Normalizando conteúdo para o inglês e finalizando tela de home

--- recomendation/urls.py
+++ recomendation/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('recomendation.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
 
 )
--- zezaz/urls.py
+++ zezaz/urls.py
@@ -9,7 +9,6 @@ admin.autodiscover()
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='recomendation/index.html'), name="index"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('recomendation.urls', namespace='recomendation'))
 
