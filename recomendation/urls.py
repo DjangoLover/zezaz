d9787ef22d3e9ec2aba52118c497d4a1dad1226b
Ezequiel Bertti <ebertti@gmail.com>
2013-09-29 02:33:52 -0300

    remove login

--- recomendation/urls.py
+++ recomendation/urls.py
@@ -5,7 +5,6 @@ from recomendation.views.login import OAuthLoginNewView
 
 urlpatterns = patterns('recomendation.views',
     url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
-    url(r'^login$', 'login', name="login"),
     url(r'^about$', TemplateView.as_view(template_name='about.html'), name="about"),
     url(r'^team$', TemplateView.as_view(template_name='team.html'), name="team"),
 
