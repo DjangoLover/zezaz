f046af8ad4fc559e7821a51fa2718337f0ce43d0
joaoleite <joaoleite@gmail.com>
2013-09-29 02:16:59 -0300

    add feedback message on following

--- zezaz/urls.py
+++ zezaz/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('recomendation.urls', namespace='recomendation')),
+    url(r'^follow/', TemplateView.as_view(template_name="follow.html")),
 
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout')
 )
